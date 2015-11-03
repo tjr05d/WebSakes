876b41a4a2ab76eb165f3d762fc02a444d6ed43d
Henry Arbolaez <henryarbolaez@gmail.com>
2015-11-03 18:22:29 -0500

    add password_digest and flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
+
   def new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:succes] = "Welcome"
+      redirect_to @user
+    else
+      flass[:danger] = "Faillll......"
+      render 'new'
+  end
+
+
+  private
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email,
+                                   :password, :password_confirmation)
+    end
 end
