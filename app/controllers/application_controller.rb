9fd864ad777d4379d50e1fd72fea26a32be982c0
tjr05d <tjr05d@my.fsu.edu>
2015-11-04 19:56:18 -0500

    A random user function was added to the application controller that selects a random user from the Users database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+
+    private
+#random_user is a method that selects a random user from the database to
+#present to the current user
+    def random_user
+    User.order("RANDOM()").first
+    end
+#the helper method allows the method to be used in multiple places 
+    helper_method :random_user
 end
