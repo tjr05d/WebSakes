7f727b1b4c7167b9844a60ecc3a96399b1b8878e
Stefanie Knapp <sknapp0125@gmail.com>
2015-11-12 15:59:30 -0500

    Generated Event Comments Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151112003533) do
+ActiveRecord::Schema.define(version: 20151112205512) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["event_id"], name: "index_comments_on_event_id"
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
