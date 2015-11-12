f63c797d996d2606715a6a36bda8d2d219798c5e
Stefanie Knapp <sknapp0125@gmail.com>
2015-11-12 15:32:39 -0500

    Create, Show, List, Update, and Destroy Events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -26,12 +26,28 @@ class EventsController < ApplicationController
     end
   end
 
+  def update
+    @event = Event.find(params[:id])
+
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+
+    redirect_to events_path
+  end
+
   private
   def event_params
     params.require(:event).permit(:title, :text)
   end
 
 end
-
 
 
