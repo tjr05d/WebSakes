68c6fa28c91e48cd61765baa7ec2ecd4a1df255b
Henry Arbolaez <henryarbolaez@gmail.com>
2015-11-13 15:47:05 -0500

    Finish styling the page added method.now to flash in the match controller,

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
-      redirect_to matches_show_path
+      redirect_to match_path
       # Handle a successful update.
     else
       render 'edit'
--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -1,3 +1,2 @@
 module MatchesHelper
-
 end
