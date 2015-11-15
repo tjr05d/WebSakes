8a84ce1e839c70b174f7ba877a664f7f89ace46a
Henry Arbolaez <henryarbolaez@gmail.com>
2015-11-15 10:17:22 -0500

    Fix teh swipecard issue with javascript, style the nav, gravatar default pic, about page

--- app/assets/javascripts/card/swipecard.js
+++ app/assets/javascripts/card/swipecard.js
@@ -1,39 +1,40 @@
+  $(document).on('ready page:load', function () {
 // cardwipe effect
-(function(){
+  // (function(){
 
-  var all = 0;
-  var results = document.querySelector('#results');
-  var counter = document.querySelector('#counter');
+    var all = 0;
+    var results = document.querySelector('#results');
+    var counter = document.querySelector('#counter');
 
-  function updatecounter() {
-    --all;
-    counter.innerHTML = all;
-  }
-
-  document.body.addEventListener('yepcard', function(ev) {
-    results.innerHTML += '<li>'+ev.detail.card.innerHTML+'</li>';
-    updatecounter();
-  });
-
-  document.body.addEventListener('nopecard', function(ev) {
-    updatecounter();
-  });
-
-  document.body.addEventListener('deckempty', function(ev) {
-    results.classList.add('live');
-    ev.detail.container.style.display = 'none';
-  });
-
-  window.addEventListener('load', function(ev) {
-    if ('content' in document.createElement('template')) {
-      var t = document.querySelector('template');
-      var list = t.parentNode;
-      var contents = t.innerHTML;
-      list.removeChild(t);
-      list.innerHTML += contents;
+    function updatecounter() {
+      --all;
+      counter.innerHTML = all;
     }
-    all = document.body.querySelectorAll('.card').length + 1;
-    updatecounter();
-  });
 
-})();
+    document.body.addEventListener('yepcard', function(ev) {
+      results.innerHTML += '<li>'+ev.detail.card.innerHTML+'</li>';
+      updatecounter();
+    });
+
+    document.body.addEventListener('nopecard', function(ev) {
+      updatecounter();
+    });
+
+    document.body.addEventListener('deckempty', function(ev) {
+      results.classList.add('live');
+      ev.detail.container.style.display = 'none';
+    });
+
+    // window.addEventListener('load', function(ev) {
+    //   if ('content' in document.createElement('template')) {
+    //     var t = document.querySelector('template');
+    //     var list = t.parentNode;
+    //     var contents = t.innerHTML;
+    //     list.removeChild(t);
+    //     list.innerHTML += contents;
+    //   }
+    //   all = document.body.querySelectorAll('.card').length + 1;
+    //   updatecounter();
+    // });
+  // })();
+});
